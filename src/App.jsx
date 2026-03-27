70b96f28511f0aedb810e50db7e007ccba97086c
Abhishek Anand <abhishek.anand@bounteous.com>
2026-03-28 04:22:35 +0530

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
 
     return (
         <div className="app-container">
-            <h1>SDE Prep Checklist All in One 🧑‍💻</h1>
+            <h1>SDE Prep Checklist🧑‍💻</h1>
             <ProgressBar value={calculateOverallProgress()} />
 
             <div className="filter-controls">
